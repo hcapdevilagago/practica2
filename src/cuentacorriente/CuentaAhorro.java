a499130b2637121366975ede4604931a7007ab4f
Alumno <Alumno@INF709>
2017-05-03 13:20:08 +0200

    cambio comentario

--- src/cuentacorriente/CuentaAhorro.java
+++ src/cuentacorriente/CuentaAhorro.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/** Cambio Berta **/
 package cuentacorriente;
 
 public class CuentaAhorro extends CuentaCorriente {
